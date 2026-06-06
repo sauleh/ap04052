169e6023674fcfb971cd9aff3fa91f83cc312111
Sauleh Eetemadi <sauleh@gmail.com>
2026-06-06 17:33:09 +0400

    u

---
+++ s24/s24con/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace s24con2;
+
+class InOutTask<Tin,Tout>
+{
+    public static int task_count = 0;
+    public Tin input {get; set;}
+    public Tout output {get; set;}
+}
+
+class Program
+{
+    static AutoResetEvent tasks_done = new AutoResetEvent(false);
+
+    static void DoWork(object obj)
+    {
+        InOutTask<int,int> task = (InOutTask<int,int>) obj;
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: start-{task.input}");
+        Thread.Sleep(100);
+        task.output = task.input * 2;
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: end-{task.input}");
+        if (0 == Interlocked.Decrement(ref InOutTask<int,int>.task_count))
+            tasks_done.Set();
+
+    }
+
+    static void Main2(string[] args)
+    {
+        List<InOutTask<int,int>> tasks = new List<InOutTask<int, int>>();
+        for(int i=0; i<40; i++)
+        {
+            var t = new InOutTask<int,int>() {input = i};
+            Interlocked.Increment(ref InOutTask<int,int>.task_count);
+            tasks.Add(t);
+            ThreadPool.QueueUserWorkItem(DoWork, t);
+        }
+
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: main-wait");
+        tasks_done.WaitOne();
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: main-done");
+    }
+}
---
+++ s24/s24con/Program2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+
+namespace s24con;
+
+class Program
+{
+    static async Task<int> DoWork(int n)
+    {
+        return 5;
+    }
+    static void Main(string[] args)
+    {
+        Task<int> t = new Task<int>( o =>
+        {
+            int n = (int) o;
+            System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: t1 start");
+            Thread.Sleep(500);
+            System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: t1 done");
+            return n * 2;
+        }, 5);
+        var tnew = t.ContinueWith(t2 => {
+            System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: t2 start");
+            Thread.Sleep(500);
+            System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: t2 done");
+            return t2.Result * 5;
+        });
+        t.Start();
+        tnew.Wait();
+        System.Console.WriteLine(t.Result);
+
+        //Console.ReadLine();
+        // Task<int> t = new Task<int>( o => Random.Shared.Next(), null);
+        // t.Start();
+        // t.Wait();
+        // System.Console.WriteLine(t.Result);
+    }
+
+
+    static void Main33(string[] args)
+    {
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: main-start");
+        var tasks = 
+            Enumerable.Range(1,25).Select(n => new Task<int>(o =>
+                    {
+                        int n = (int) o;
+                        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: start");
+                        Thread.Sleep(500);
+                        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: end");            
+                        return n * 2;                        
+                    }, n))
+                    .ToList();
+        tasks.ForEach(t => t.Start());
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: main-waiting");
+
+        Task.WaitAll(tasks);        
+        tasks.ForEach(t => System.Console.WriteLine(t.Result));
+        System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: main-finished");        
+    }
+}
